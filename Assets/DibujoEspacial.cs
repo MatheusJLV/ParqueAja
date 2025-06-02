a2a07dbde71b41379531d4414fd9655d6c37206b
matheusjlv <matheusjlv@gmail.com>
2025-06-02 16:18:13 -0500

    finalizacion de dibujo con espejo. se planea seguir con dibujo magnetico

--- Assets/DibujoEspacial.cs
+++ Assets/DibujoEspacial.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.XR;
 using CommonUsages = UnityEngine.XR.CommonUsages;
 using InputDevice = UnityEngine.XR.InputDevice;
+
+/*
+ * Espejo:
+ * Se puede flippear con escalado -1 en los distintos ejes Y y Z
+ */
 
 public class DibujoEspacial : MonoBehaviour
 {
@@ -25,6 +31,93 @@ public class DibujoEspacial : MonoBehaviour
 
     private List<GameObject> drawnLines = new List<GameObject>();
     private List<GameObject> drawnLines2 = new List<GameObject>();
+
+    // Variables para flippear el espejo
+    public bool YBool = false;
+    public bool ZBool = false;
+    public GameObject espejo;
+
+    public Toggle yToggle;
+    public Toggle zToggle;
+
+    public Slider colorSlider;
+    public Slider colorSlider2;
+
+    private Color currentColor = Color.red; // Color inicial
+    private Color currentColor2 = Color.blue; // Color inicial
+
+    public Image colorSliderFill;
+    public Image colorSlider2Fill;
+
+    public Material lineMaterial;
+    public Material lineMaterial2;
+
+
+
+
+    void Start()
+    {
+        if (yToggle != null)
+            yToggle.onValueChanged.AddListener(SetYBool);
+
+        if (zToggle != null)
+            zToggle.onValueChanged.AddListener(SetZBool);
+
+        if (colorSlider != null)
+            colorSlider.onValueChanged.AddListener(OnColorSliderChanged);
+        if (colorSlider2 != null)
+            colorSlider2.onValueChanged.AddListener(OnColorSliderChanged2);
+
+    }
+
+    void OnColorSliderChanged(float value)
+    {
+        currentColor = Color.HSVToRGB(value, 1f, 1f);
+        if (colorSliderFill != null)
+            colorSliderFill.color = currentColor;
+        if (lineMaterial != null)
+            lineMaterial.color = currentColor;
+    }
+
+
+    void OnColorSliderChanged2(float value)
+    {
+        currentColor2 = Color.HSVToRGB(value, 1f, 1f);
+        if (colorSlider2Fill != null)
+            colorSlider2Fill.color = currentColor2;
+        if (lineMaterial2 != null)
+            lineMaterial2.color = currentColor2;
+    }
+
+    void OnDestroy()
+    {
+        if (yToggle != null)
+            yToggle.onValueChanged.RemoveListener(SetYBool);
+
+        if (zToggle != null)
+            zToggle.onValueChanged.RemoveListener(SetZBool);
+    }
+
+    public void FlipEspejo()
+    {
+        if (espejo == null) return;
+
+        float yScale = YBool ? -1f : 1f;
+        float zScale = ZBool ? -1f : 1f;
+        espejo.transform.localScale = new Vector3(1f, yScale, zScale);
+    }
+
+    public void SetYBool(bool value)
+    {
+        YBool = value;
+        FlipEspejo();
+    }
+
+    public void SetZBool(bool value)
+    {
+        ZBool = value;
+        FlipEspejo();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -155,87 +248,6 @@ public class DibujoEspacial : MonoBehaviour
         prevLeftSecondary = leftSecondaryPressed;
     }
 
-    /*void Update()
-    {
-        if (!canDraw) return;
-
-        // Obtén los dispositivos de las manos derecha e izquierda
-        var rightHandDevices = new List<InputDevice>();
-        var leftHandDevices = new List<InputDevice>();
-        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandDevices);
-        InputDevices.GetDevicesAtXRNode(XRNode.LeftHand, leftHandDevices);
-
-        // Variables para verificar los estados de los botones
-        bool rightPrimaryPressed = false;
-        bool rightSecondaryPressed = false;
-        bool leftPrimaryPressed = false;
-        bool leftSecondaryPressed = false;
-
-        // Verifica los botones del controlador derecho
-        foreach (var device in rightHandDevices)
-        {
-            device.TryGetFeatureValue(CommonUsages.primaryButton, out rightPrimaryPressed);
-            device.TryGetFeatureValue(CommonUsages.secondaryButton, out rightSecondaryPressed);
-        }
-
-        // Verifica los botones del controlador izquierdo
-        foreach (var device in leftHandDevices)
-        {
-            device.TryGetFeatureValue(CommonUsages.primaryButton, out leftPrimaryPressed);
-            device.TryGetFeatureValue(CommonUsages.secondaryButton, out leftSecondaryPressed);
-        }
-
-        if (rightPrimaryPressed)
-        {
-            if (!isDrawing)
-            {
-                StartLine();
-            }
-
-            Vector3 currentPos = drawingTipRight.position;
-            if (Vector3.Distance(currentPos, lastPoint) > minDistance)
-            {
-                AddPoint(currentPos);
-                lastPoint = currentPos;
-            }
-        }
-        else if (isDrawing)
-        {
-            EndLine();
-        }
-
-        if (rightSecondaryPressed)
-        {
-            if (!isDrawing2)
-            {
-                StartLine2();
-            }
-
-            Vector3 currentPos = drawingTipLeft.position;
-            if (Vector3.Distance(currentPos, lastPoint2) > minDistance)
-            {
-                AddPoint2(currentPos);
-                lastPoint2 = currentPos;
-            }
-        }
-        else if (isDrawing2)
-        {
-            EndLine2();
-        }
-
-        if (leftSecondaryPressed)
-        {
-            ClearAllLines2();
-        }
-        if (leftPrimaryPressed)
-        {
-            ClearAllLines();
-        }
-
-    }*/
-
-
-
     void StartLine()
     {
         GameObject lineObj = Instantiate(linePrefab);
@@ -246,6 +258,7 @@ public class DibujoEspacial : MonoBehaviour
         lastPoint = drawingTipRight.position;
         isDrawing = true;
     }
+
 
     void AddPoint(Vector3 point)
     {
@@ -276,7 +289,7 @@ public class DibujoEspacial : MonoBehaviour
         drawnLines2.Add(lineObj);
         currentLine2.positionCount = 1;
         currentLine2.SetPosition(0, drawingTipLeft.position);
-        lastPoint = drawingTipLeft.position;
+        lastPoint2 = drawingTipLeft.position;
         isDrawing2 = true;
     }
 
